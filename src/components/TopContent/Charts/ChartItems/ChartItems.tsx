45cae934fc7ee889f48dc63ffc292b4dde4d6002
daniel_eln <omoniyiopemipo6@gmail.com>
2022-12-14 16:55:34 +0100

    made API request to get data for chartItems.

--- src/components/TopContent/Charts/ChartItems/ChartItems.tsx
+++ src/components/TopContent/Charts/ChartItems/ChartItems.tsx
@@ -1,6 +1,6 @@
-import { prependOnceListener } from "process";
 import React, {useState, useEffect} from "react";
-import {useLocation} from "react-router-dom";
+import axios from "axios";
+import Spinner from "../../../UI/Spinner/Spinner";
 import ChartItem from "./ChartItem/ChartItem";
 import classes from "./ChartItems.module.css";
 
@@ -12,49 +12,41 @@ import classes from "./ChartItems.module.css";
 // }
 
 const ChartItems = (props: any) => {
-    const [chart, setChart] = useState<any>([
-        {
-            songName: "Golden age of 80s",
-            songArtist: "Sean swadder",
-            imgSrc: "chartImages/chart-img-1.png",
-            length: "2:34:45",
-            id: "playlist-1"
-        },
-        {
-            songName: "Raggae 'n' blues",
-            songArtist: "Dj YK mule",
-            imgSrc: "chartImages/chart-img-2.png",
-            length: "1:02:42",
-            id: "playlist-2"
-        },
-        {
-            songName: "Tomorrow's tunes",
-            songArtist: "Obi Datti",
-            imgSrc: "chartImages/chart-img-3.png",
-            length: "2:01:25",
-            id: "playlist-3"
-        }
-    ]);
+    const [chart, setChart] = useState<any>([]);
 
-    // useEffect(() => {
-    //     console.log("[ChartItems.js] useEffect ran"); 
-    //     console.log(chart);
-    // }, []);
-    
-    return (
-        <div className={classes.Container}>
+    useEffect(() => {
+        // Trigger the API Call
+        axios.get(`https://musica-api.onrender.com/playlist`)
+            .then(res => {
+                console.log(res.status);
+                setChart(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }, []);
+
+    const DOM = (
+        <React.Fragment>
             {/* use short-circuiting to infer if the chart is available before mapping the array */}
             {chart && chart.map((song: any)=> {
                 return <ChartItem 
-                    title = {song.songName}
-                    artist = {song.songArtist}
-                    imgSrc = {song.imgSrc}
-                    length = {song.length}
-                    id =  {song.id}
+                    title = {song["title"]}
+                    // Take name of first artist in the playlist
+                    artist = {song["files"][0]["artist"]}
+                    imgSrc = {song["cover"]}
+                    length = {"30:00"}
+                    id =  {song["id"]}
                     // Recall, key must be added to any dynamically used array for content display.
-                    key = {song.length}
+                    key = {song["id"]}
                 />
             })}
+        </React.Fragment>
+    )
+    
+    return (
+        <div className={classes.Container}>
+            {chart.length == 0? <Spinner />: DOM}
         </div>
     );
 }
